619720d3fe51ee6401e3ff0fefd9d828e05de62c
fooshnip <riley.jos@gmail.com>
2013-04-24 11:07:56 -0500

    music player

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#jquery_jplayer_1").jPlayer({
     ready: function () {
       $(this).jPlayer("setMedia", {
-      mp3:"../../assets/music/smooth.mp3"
+      mp3:"http://c1.glitch.bz/wp-uploads/main/2012/08/waiting_muzak_1344997267.mp3"
     });
   },
       swfPath: "js",
